99631a8aba1fdb38f38f4f4c4999a6cd51cf1fce
ifouche <ifouche10@gmail.com>
2022-05-18 20:46:30 -0400

    Final Push

--- Level/Level.gd
+++ Level/Level.gd
@@ -19,7 +19,6 @@ export (int,2,10) var number_of_bubble_types = 4;
 func _ready():
 	randomize();
 	PlayerLocation = Player.global_position;
-	print(PlayerLocation)
 	set_bubbles();
 	check_remaining_bubbles();
 	
@@ -36,8 +35,13 @@ func _on_Player_fire_bubble(angle):
 	
 func check_remaining_bubbles():
 	var available_bubbles = [];
+	var bubble_type_left = [];
+	for cell in Bubbles.get_used_cells():
+		bubble_type_left.append(Bubbles.get_cellv(cell));
+
 	for bubble in number_of_bubble_types:
-		available_bubbles.append(bubble);
+		if(bubble_type_left.has(bubble)):
+			available_bubbles.append(bubble);
 		
 	NextBubble.prepare_bubble(available_bubbles);
 
@@ -76,3 +80,28 @@ func _on_RemovalTimer_timeout():
 	if matching_bubbles.size() > 2:
 		for bubble in matching_bubbles:
 			Bubbles.set_cellv(bubble, -1);
+	check_for_detached_bubbles();
+	
+func check_for_detached_bubbles():
+	var bubbles = Bubbles.get_used_cells();
+	var bubbles_to_drop = [];
+	var attached_bubbles = [];
+	
+	for bubble in bubbles:
+		if bubble.y == 0:
+			attached_bubbles.append(bubble);
+			
+	for bubble in bubbles:
+		var neighbors = get_neighbors(bubble);
+		for n in neighbors:
+			if attached_bubbles.has(n+bubble):
+				attached_bubbles.append(bubble);
+				break;
+	
+	for bubble in bubbles:
+		print(attached_bubbles.has(bubble))
+		if not attached_bubbles.has(bubble):
+			Bubbles.set_cellv(bubble, -1);
+		
+	
+	
